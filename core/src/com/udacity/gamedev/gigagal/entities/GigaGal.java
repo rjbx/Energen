9f20f920fd0f2934e23a3fbe090e7829c8892a34
rjbx <rjbx@users.noreply.github.com>
2016-10-29 10:58:38 -0400

    updated sprites

--- core/src/com/udacity/gamedev/gigagal/entities/GigaGal.java
+++ core/src/com/udacity/gamedev/gigagal/entities/GigaGal.java
@@ -4,7 +4,6 @@ import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.Input.Keys;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.graphics.g2d.TextureRegion;
-import com.badlogic.gdx.graphics.g3d.environment.SphericalHarmonics;
 import com.badlogic.gdx.math.Rectangle;
 import com.badlogic.gdx.math.Vector2;
 import com.badlogic.gdx.utils.Array;
