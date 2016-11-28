219cc7e7e7af000e87755a828fcd31ee7e92b8f8
rjbx <rjbx@users.noreply.github.com>
2016-11-28 14:28:46 -0500

    resized flame asset

--- core/src/com/udacity/gamedev/gigagal/util/Constants.java
+++ core/src/com/udacity/gamedev/gigagal/util/Constants.java
@@ -87,7 +87,7 @@ public final class Constants {
     public static final Vector2 FLAME_CENTER = new Vector2(4, 8);
     public static final int FLAME_DAMAGE = 25;
     public static final float FLAME_COLLISION_WIDTH = 15;
-    public static final float FLAME_COLLISION_HEIGHT = 27;
+    public static final float FLAME_COLLISION_HEIGHT = 25;
     public static final String FLAME_SPRITE = "flame";
 
     // Ammo
