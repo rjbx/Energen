e36855ba08ec7037b44a25e3c02aa17b67ef9752
rjbx <rjbx@users.noreply.github.com>
2016-12-02 16:57:50 -0500

    refactored level-specific platform names to 'platform-[level]'

--- core/src/com/udacity/gamedev/gigagal/util/Constants.java
+++ core/src/com/udacity/gamedev/gigagal/util/Constants.java
@@ -64,13 +64,13 @@ public final class Constants {
     public static final int PLATFORM_EDGE = 8;
     public static final float MAX_LEDGE_HEIGHT = 5;
     public static final String PLATFORM_SPRITE = "platform";
-    public static final String FIRE_PLATFORM_SPRITE = "fire-platform";
-    public static final String WATER_PLATFORM_SPRITE = "water-platform";
-    public static final String ELECTRIC_PLATFORM_SPRITE = "electric-platform";
-    public static final String METAL_PLATFORM_SPRITE = "metal-platform";
-    public static final String RUBBER_PLATFORM_SPRITE = "rubber-platform";
-    public static final String PSYCHIC_PLATFORM_SPRITE = "psychic-platform";
-    public static final String FINAL_PLATFORM_SPRITE = "final-platform";
+    public static final String FIRE_PLATFORM_SPRITE = "platform-fire";
+    public static final String WATER_PLATFORM_SPRITE = "platform-water";
+    public static final String ELECTRIC_PLATFORM_SPRITE = "platform-electric";
+    public static final String METAL_PLATFORM_SPRITE = "platform-metal";
+    public static final String RUBBER_PLATFORM_SPRITE = "platform-rubber";
+    public static final String PSYCHIC_PLATFORM_SPRITE = "platform-psychic";
+    public static final String FINAL_PLATFORM_SPRITE = "platform-final";
 
 
     // Zoomba
