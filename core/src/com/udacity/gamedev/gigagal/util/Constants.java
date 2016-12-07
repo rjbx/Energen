f27e09c004e7dd96e1618b4e88ebe808d6f4e75a
rjbx <rjbx@users.noreply.github.com>
2016-12-06 19:42:41 -0500

    updated zoomba center

--- core/src/com/udacity/gamedev/gigagal/util/Constants.java
+++ core/src/com/udacity/gamedev/gigagal/util/Constants.java
@@ -74,15 +74,15 @@ public final class Constants {
 
 
     // Zoomba
-    public static final Vector2 ZOOMBA_CENTER = new Vector2(14, 22);
+    public static final Vector2 ZOOMBA_CENTER = new Vector2(14, 25);
     public static final float ZOOMBA_MOVEMENT_SPEED = 10;
     public static final float ZOOMBA_BOB_AMPLITUDE = 2;
     public static final float ZOOMBA_BOB_PERIOD = 3.0f;
     public static final int ZOOMBA_MAX_HEALTH = 30;
     public static final int ZOOMBA_STANDARD_DAMAGE = 10;
     public static final Vector2 ZOOMBA_KNOCKBACK = new Vector2(75, VERTICAL_KNOCKBACK);
-    public static final float ZOOMBA_COLLISION_WIDTH = 25;
-    public static final float ZOOMBA_COLLISION_HEIGHT = 45;
+    public static final float ZOOMBA_COLLISION_WIDTH = 28;
+    public static final float ZOOMBA_COLLISION_HEIGHT = 50;
     public static final float ZOOMBA_SHOT_RADIUS = 17;
     public static final String ZOOMBA_SPRITE = "zoomba";
 
