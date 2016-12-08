43c3aec95e73244e4b7eb9e262541052a4ac021c
rjbx <rjbx@users.noreply.github.com>
2016-12-08 11:20:55 -0500

    updated ammo getters with orientation-based center

--- core/src/com/udacity/gamedev/gigagal/entities/Cannon.java
+++ core/src/com/udacity/gamedev/gigagal/entities/Cannon.java
@@ -12,7 +12,6 @@ public class Cannon implements Ground {
 
     // fields
     private Vector2 position;
-    private Enums.Orientation orientation;
     private TextureRegion region;
     private Vector2 center;
 
@@ -20,7 +19,6 @@ public class Cannon implements Ground {
     public Cannon(Vector2 position, Enums.Orientation orientation) {
 
         this.position = position;
-        this.orientation = orientation;
         switch (orientation) {
             case VERTICAL:
                 region = Assets.getInstance().getCannonAssets().verticalCannon;
@@ -34,16 +32,15 @@ public class Cannon implements Ground {
     }
 
     public void render(SpriteBatch batch) {
-
         Utils.drawTextureRegion(batch, region, position, center);
     }
 
     public final Vector2 getPosition() { return position; }
-    public final float getHeight() { return Constants.LATERAL_CANNON_CENTER.x * 2; }
-    public final float getWidth() { return Constants.LATERAL_CANNON_CENTER.y * 2; }
-    public final float getLeft() { return position.x - Constants.LATERAL_CANNON_CENTER.x; }
-    public final float getRight() { return position.x + Constants.LATERAL_CANNON_CENTER.x; }
-    public final float getTop() { return position.y + Constants.LATERAL_CANNON_CENTER.y; }
-    public final float getBottom() { return position.y - Constants.LATERAL_CANNON_CENTER.y; }
+    public final float getHeight() { return center.x * 2; }
+    public final float getWidth() { return center.y * 2; }
+    public final float getLeft() { return position.x - center.x; }
+    public final float getRight() { return position.x + center.x; }
+    public final float getTop() { return position.y + center.y; }
+    public final float getBottom() { return position.y - center.y; }
     public final Class getSubclass() { return this.getClass(); }
 }
