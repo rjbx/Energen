617bc597fd7dcfce5216ef091c145c8e9e83f592
rjbx <rjbx@users.noreply.github.com>
2016-12-08 08:35:18 -0500

    updated change weapon delay as well as vacuum and coil knockback constants

--- core/src/com/udacity/gamedev/gigagal/util/Constants.java
+++ core/src/com/udacity/gamedev/gigagal/util/Constants.java
@@ -154,7 +154,7 @@ public final class Constants {
     public static final Vector2 COIL_CENTER = new Vector2(12.5f, 12.5f);
     public static final float COIL_DURATION = 0.5f;
     public static final int COIL_DAMAGE = 15;
-    public static final Vector2 COIL_KNOCKBACK = new Vector2(100, 0);
+    public static final Vector2 COIL_KNOCKBACK = new Vector2(200, 50);
     public static final float COIL_COLLISION_WIDTH = 25;
     public static final float COIL_COLLISION_HEIGHT = 25;
     public static final String COIL_SPRITE_1 = "coil-1";
@@ -164,7 +164,7 @@ public final class Constants {
     public static final Vector2 VACUUM_CENTER = new Vector2(31, 31);
     public static final float VACUUM_FRAME_DURATION = 0.4f;
     public static final int VACUUM_DAMAGE = 100;
-    public static final Vector2 VACUUM_KNOCKBACK = new Vector2(100, 0);
+    public static final Vector2 VACUUM_KNOCKBACK = new Vector2(100, 10);
     public static final float VACUUM_COLLISION_WIDTH = 25;
     public static final float VACUUM_COLLISION_HEIGHT = 25;
     public static final String VACUUM_SPRITE_1 = "vacuum-1";
@@ -189,7 +189,7 @@ public final class Constants {
     public static final int AMMO_SPECIALIZED_DAMAGE = 30;
     public static final Vector2 SHOT_CENTER = new Vector2(3, 2);
     public static final Vector2 BLAST_CENTER = new Vector2(8, 4);
-    public static final float WEAPON_TOGGLER_DELAY = 0.5f;
+    public static final float WEAPON_TOGGLER_DELAY = 0.2f;
     public static final String SHOT_NATIVE_SPRITE = "ammo-shot-native";
     public static final String BLAST_NATIVE_SPRITE = "ammo-blast-native";
     public static final String SHOT_FIRE_SPRITE = "ammo-shot-fire";
