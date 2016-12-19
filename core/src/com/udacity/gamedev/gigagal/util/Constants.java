7852670591e8b91e296fdb483bd697fc1ca18cc7
rjbx <rjbx@users.noreply.github.com>
2016-12-19 15:37:45 -0500

    updated hover and removed old ricochet sprites

--- core/src/com/udacity/gamedev/gigagal/util/Constants.java
+++ core/src/com/udacity/gamedev/gigagal/util/Constants.java
@@ -316,7 +316,7 @@ public final class Constants {
     public static final String BLAST_ICON = "icon-blast";
     public static final String JUMP_ICON = "icon-jump";
     public static final String HOVER_ICON = "icon-hover";
-    public static final String RICOCHET_ICON = "icon-ricochet3";
+    public static final String RICOCHET_ICON = "icon-ricochet";
     public static final String DASH_ICON = "icon-dash";
 
     // Onscreen Controls
