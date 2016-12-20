5aca83292c1f84f88d5b551c7ffe6c829cc1764b
rjbx <rjbx@users.noreply.github.com>
2016-12-20 10:41:49 -0500

    attempting to adjust gauge bar according to gigagal gauge levels

--- core/src/com/udacity/gamedev/gigagal/overlays/GaugeHud.java
+++ core/src/com/udacity/gamedev/gigagal/overlays/GaugeHud.java
@@ -60,18 +60,18 @@ public final class GaugeHud {
         // health
         renderer.setColor(Color.SKY);
         renderer.set(ShapeRenderer.ShapeType.Filled);
-        renderer.rect(viewport.getScreenX(), viewport.getWorldHeight() - Constants.HUD_MARGIN, viewport.getWorldWidth() / 3, viewport.getScreenHeight() / 25);
+        renderer.rect(viewport.getScreenX(), viewport.getWorldHeight() - Constants.HUD_MARGIN, (gigaGal.getHealth() / 100) * viewport.getWorldWidth() / 3, viewport.getScreenHeight() / 25);
 
         // turbo
         renderer.setColor(Color.LIME);
         renderer.set(ShapeRenderer.ShapeType.Filled);
-        renderer.rect(viewport.getWorldWidth() / 3, viewport.getWorldHeight() - Constants.HUD_MARGIN, viewport.getWorldWidth() / 3, viewport.getScreenHeight() / 25);
+        renderer.rect(viewport.getWorldWidth() / 3, viewport.getWorldHeight() - Constants.HUD_MARGIN, (gigaGal.getTurbo() / 100) * viewport.getWorldWidth() / 3, viewport.getScreenHeight() / 25);
 
 
         // ammo
         renderer.setColor(Color.CORAL);
         renderer.set(ShapeRenderer.ShapeType.Filled);
-        renderer.rect(viewport.getWorldWidth() / 3 * 2, viewport.getWorldHeight() - Constants.HUD_MARGIN, viewport.getWorldWidth() / 3, viewport.getScreenHeight() / 25);
+        renderer.rect(viewport.getWorldWidth() / 3 * 2, viewport.getWorldHeight() - Constants.HUD_MARGIN, (gigaGal.getAmmo() / 100) * viewport.getWorldWidth() / 3, viewport.getScreenHeight() / 25);
 
         renderer.end();
 
