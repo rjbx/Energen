e9ac2bc7a8d76fd416e239af6b6c2a5dfb13b04a
rjbx <rjbx@users.noreply.github.com>
2017-05-24 15:58:48 -0400

    restored orben ground collision handling to check for box instad of climbable

--- core/src/com/udacity/gamedev/gigagal/entity/Orben.java
+++ core/src/com/udacity/gamedev/gigagal/entity/Orben.java
@@ -119,7 +119,7 @@ public class Orben implements Destructible, Hazard {
         }
 
         for (Ground ground : LevelUpdater.getInstance().getGrounds()) {
-            if (!(ground instanceof Climbable)) {
+            if (!(ground instanceof Box)) {
                 if (Helpers.overlapsBetweenFourSides(position, getWidth(), getHeight(), ground.getLeft(), ground.getRight(), ground.getBottom(), ground.getTop())) {
                     velocity.setZero();
                     position.set(previousFramePosition);
