16f0516f8ad0fb725c67eaec29df8eb98cdc7519
rjbx <rjbx@users.noreply.github.com>
2017-06-05 20:57:52 -0400

    restored setting action to stand within stand method for sinkable, skateable and rideable

--- core/src/com/udacity/gamedev/gigagal/entity/GigaGal.java
+++ core/src/com/udacity/gamedev/gigagal/entity/GigaGal.java
@@ -765,14 +765,18 @@ public class GigaGal implements Humanoid {
         } else if (touchedGround instanceof Rideable) {
             velocity.x = 0;
             velocity.x += Helpers.absoluteToDirectionalValue(Constants.TREADMILL_SPEED, ((Rideable) touchedGround).getDirection(), Orientation.X);
-        } else if (action == Action.STRIDING && Math.abs(velocity.x) > 2) {
+        } else if (action == Action.STRIDING && Math.abs(velocity.x) > 3) {
             velocity.x *= .75f;
             strideAcceleration *= .5f;
         } else {
-            action = Action.STANDING;
-            groundState = GroundState.PLANTED;
             velocity.x = 0;
         }
+
+        if (!(action == Action.STRIDING && Math.abs(velocity.x) > 3)) {
+            action = Action.STANDING;
+            groundState = GroundState.PLANTED;
+        }
+
         if (!canClimb) {
             canJump = true;
             handleYInputs(); // disabled when canclimb to prevent look from overriding climb
