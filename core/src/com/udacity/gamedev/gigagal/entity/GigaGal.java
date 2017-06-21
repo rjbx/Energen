9e76dfaeb369e02794ead50b0f591f77dfc214dc
rjbx <rjbx@users.noreply.github.com>
2017-06-21 14:40:05 -0400

    updated thermal json

--- core/src/com/udacity/gamedev/gigagal/entity/GigaGal.java
+++ core/src/com/udacity/gamedev/gigagal/entity/GigaGal.java
@@ -1,6 +1,5 @@
 package com.udacity.gamedev.gigagal.entity;
 
-import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.graphics.g2d.TextureRegion;
 import com.badlogic.gdx.math.MathUtils;
