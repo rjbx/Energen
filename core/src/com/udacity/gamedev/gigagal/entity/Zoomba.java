ea6310ee9b394aeb5efe65e9605d7b1753aefe20
rjbx <rjbx@users.noreply.github.com>
2017-07-08 14:14:27 -0400

    updated thermal json

--- core/src/com/udacity/gamedev/gigagal/entity/Zoomba.java
+++ core/src/com/udacity/gamedev/gigagal/entity/Zoomba.java
@@ -8,7 +8,6 @@ import com.badlogic.gdx.math.Vector2;
 import com.badlogic.gdx.utils.Array;
 import com.badlogic.gdx.utils.TimeUtils;
 import com.badlogic.gdx.utils.viewport.Viewport;
-import com.udacity.gamedev.gigagal.app.LevelUpdater;
 import com.udacity.gamedev.gigagal.util.Assets;
 import com.udacity.gamedev.gigagal.util.Constants;
 import com.udacity.gamedev.gigagal.util.Enums;
