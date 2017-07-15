d7c3f96ce24207962aa2f6d43d7e8939fea57b75
rjbx <rjbx@users.noreply.github.com>
2017-07-15 17:26:44 -0400

    set portable toss parameter to add gigagal directional ground width value

--- core/src/com/udacity/gamedev/gigagal/app/LevelUpdater.java
+++ core/src/com/udacity/gamedev/gigagal/app/LevelUpdater.java
@@ -307,7 +307,7 @@ public class LevelUpdater {
             } else if (((Portable) ground).getCarrier() == gigaGal && !InputControls.getInstance().shootButtonPressed) {
                 ((Portable) ground).setCarrier(null);
                 if (InputControls.getInstance().leftButtonPressed || InputControls.getInstance().rightButtonPressed) {
-                    ((Portable) ground).toss(gigaGal.getVelocity().x);
+                    ((Portable) ground).toss(gigaGal.getVelocity().x + Helpers.absoluteToDirectionalValue(ground.getWidth() * 2, gigaGal.getDirectionX(), Enums.Orientation.X));
                 }
             }
         }
