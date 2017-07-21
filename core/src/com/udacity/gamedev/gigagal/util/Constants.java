a409fd06132ad65ae48e14a99b45e43e36ee2fae
rjbx <rjbx@users.noreply.github.com>
2017-07-20 21:12:10 -0400

    increased tripspring load speed

--- core/src/com/udacity/gamedev/gigagal/util/Constants.java
+++ core/src/com/udacity/gamedev/gigagal/util/Constants.java
@@ -279,7 +279,7 @@ public final class Constants {
     public static final String LEVER_SPRITE_3 = "lever-3";
     public static final String LEVER_SPRITE_4 = "lever-4";
     public static final String LEVER_SPRITE_5 = "lever-5";
-    public static final float LEVER_LOAD_DURATION = 0.75f;
+    public static final float LEVER_LOAD_DURATION = 0.25f;
     public static final float LEVER_UNLOAD_DURATION = 0.75f;
 
     // Sand
