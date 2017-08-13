7099133b219729a268ac7fc2fe38db45ee3bac42
rjbx <rjbx@users.noreply.github.com>
2017-08-13 12:35:42 -0400

    enabled rappel from spring by limiting touchgroundtop for moving grounds to planted ground state

--- core/src/com/udacity/gamedev/gigagal/entity/GigaGal.java
+++ core/src/com/udacity/gamedev/gigagal/entity/GigaGal.java
@@ -639,20 +639,22 @@ public class GigaGal extends Entity implements Humanoid {
                     }
                 }
                 if (g instanceof Moving) {
-                    Moving moving = (Moving) g;
-                    if (!moving.getVelocity().equals(Vector2.Zero)) {
-                        lookStartTime = 0;
-                    }
-                    position.y = g.getTop() + Constants.GIGAGAL_EYE_HEIGHT;
-                    velocity.x = ((Moving) g).getVelocity().x;
-                    velocity.y = ((Moving) g).getVelocity().y;
-                    if (moving instanceof Pliable && ((Pliable) moving).isAtopMovingGround() && (touchedGround == null || !touchedGround.equals(((Pliable) moving).getMovingGround()))) { // atop pliable which is atop moving ground and not simultaneously touching both
-                        Pliable pliable = (Pliable) moving;
-                        if (!pliable.isBeingCarried() && directionY == Direction.DOWN && lookStartTime != 0) {
-                            if (InputControls.getInstance().shootButtonJustPressed) {
-                                fall();
+                    if (groundState == GroundState.PLANTED) {
+                        Moving moving = (Moving) g;
+                        if (!moving.getVelocity().equals(Vector2.Zero)) {
+                            lookStartTime = 0;
+                        }
+                        position.y = g.getTop() + Constants.GIGAGAL_EYE_HEIGHT;
+                        velocity.x = ((Moving) g).getVelocity().x;
+                        velocity.y = ((Moving) g).getVelocity().y;
+                        if (moving instanceof Pliable && ((Pliable) moving).isAtopMovingGround() && (touchedGround == null || !touchedGround.equals(((Pliable) moving).getMovingGround()))) { // atop pliable which is atop moving ground and not simultaneously touching both
+                            Pliable pliable = (Pliable) moving;
+                            if (!pliable.isBeingCarried() && directionY == Direction.DOWN && lookStartTime != 0) {
+                                if (InputControls.getInstance().shootButtonJustPressed) {
+                                    fall();
+                                }
+                                canMove = true;
                             }
-                            canMove = true;
                         }
                     }
                 }
