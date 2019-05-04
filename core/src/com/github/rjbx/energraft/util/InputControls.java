b9e84810ea134e9e41767dd8114df0d0fb6f4495
rjbx <rjbx@users.noreply.github.com>
2019-05-04 19:59:22 -0400

    refactor: Revert key reassignment

--- core/src/com/github/rjbx/energraft/util/InputControls.java
+++ core/src/com/github/rjbx/energraft/util/InputControls.java
@@ -196,7 +196,7 @@ public class InputControls extends InputAdapter {
         if (Gdx.input.isKeyPressed(Input.Keys.D)) {
             rightButtonPressed = true;
         }
-        if (Gdx.input.isKeyPressed(Input.Keys.SHIFT_RIGHT)) {
+        if (Gdx.input.isKeyPressed(Input.Keys.UP)) {
             jumpButtonPressed = true;
         }
         if (Gdx.input.isKeyPressed(Input.Keys.ENTER)) {
