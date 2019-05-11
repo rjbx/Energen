be878fc50ae58682207fa9fa44b79d9c2081d12b
rjbx <rjbx@users.noreply.github.com>
2019-05-11 13:44:55 -0400

    refactor: Apply turbo multiplier on decrement

--- core/src/com/github/rjbx/energen/entity/Avatar.java
+++ core/src/com/github/rjbx/energen/entity/Avatar.java
@@ -380,7 +380,7 @@ public class Avatar extends Entity implements Impermeable, Humanoid {
             } else if (swipeTimeSeconds < Constants.FLIPSWIPE_FRAME_DURATION * 5) {
                 AssetManager.getInstance().getSoundAssets().getEnergySound(energy).play();
                 swipeTimeSeconds = Helpers.secondsSince(swipeStartTime);
-                turbo -= Constants.DASH_TURBO_DECREMENT;
+                turbo -= Constants.DASH_TURBO_DECREMENT * turboMultiplier;
             } else { // auto deactivation when animation completes
                 AssetManager.getInstance().getSoundAssets().getEnergySound(energy).stop();
                 swipeStartTime = 0;
