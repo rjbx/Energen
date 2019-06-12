589a2326d2fa77c623311b26e568a7db98efff1a
rjbx <rjbx@users.noreply.github.com>
2019-06-11 22:57:42 -0400

    refactor: Update pause screen text and layout

--- core/src/com/github/rjbx/energen/app/LevelScreen.java
+++ core/src/com/github/rjbx/energen/app/LevelScreen.java
@@ -262,13 +262,13 @@ final class LevelScreen extends ScreenAdapter {
 
 
     private static void setMainMenu() {
-        cursor.setRange(staticViewport.getCamera().position.y, staticViewport.getCamera().position.y - 30);
+        cursor.setRange(staticViewport.getCamera().position.y -30, staticViewport.getCamera().position.y - 60);
         cursor.setOrientation(Enums.Orientation.Y);
         cursor.resetPosition();
         String[] optionStrings = {"RESUME", "EXIT", "OPTIONS"};
         menu.setOptionStrings(Arrays.asList(optionStrings));
         menu.setPromptString(Align.left, "GAUGE\n" + Constants.HUD_AMMO_LABEL + avatar.getAmmo() + "\n" + Constants.HUD_HEALTH_LABEL + avatar.getHealth() + "\n" + "Turbo: " + avatar.getTurbo() + "\nEnergy: " + avatar.getEnergy().toString());
-        menu.setPromptString(Align.right, "TOTAL\n" + "Game Time: " + Helpers.secondsToString((TimeUtils.nanosToMillis(SaveData.getTotalTime()) + levelUpdater.getUnsavedTime())) + "\n" + "Game Score: " + (SaveData.getTotalScore() + levelUpdater.getUnsavedScore()) + "\nLevel Time: " + Helpers.secondsToString(TimeUtils.nanosToMillis(levelUpdater.getTime())) + "\nLevel Score: " + levelUpdater.getScore());
+        menu.setPromptString(Align.right, "TOTAL\n" + "Game Time: " + Helpers.secondsToString((TimeUtils.nanosToMillis(SaveData.getTotalTime()) + levelUpdater.getUnsavedTime())) + "\nLevel Time: " + Helpers.secondsToString(TimeUtils.nanosToMillis(levelUpdater.getTime())) + "\nGame Score: " + (SaveData.getTotalScore() + levelUpdater.getUnsavedScore()) + "\nLevel Score: " + levelUpdater.getScore());
 //        menu.setPromptString(Align.right, "ENERGY\n" + (avatar.getEnergy().name().toLowerCase() + "\n" + SaveData.getEnergies().replace(avatar.getEnergy().name(), "").replace(", ", "\n")).replace("\n\n", "\n").toLowerCase());
         menu.TextAlignment(Align.center);
         menuType = Enums.MenuType.MAIN;
