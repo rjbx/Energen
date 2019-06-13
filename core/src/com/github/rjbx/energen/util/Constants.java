71dd5c5275d28a2585e23ad9d26b552cc3d4f5a1
rjbx <rjbx@users.noreply.github.com>
2019-06-13 07:06:57 -0400

    refactor: Update pause screen labels

--- core/src/com/github/rjbx/energen/util/Constants.java
+++ core/src/com/github/rjbx/energen/util/Constants.java
@@ -936,9 +936,9 @@ public final class Constants {
     public static final String DASH_ICON = "icon-dash";
     public static final String MOVE_ICON = "icon-move";
     public static final String LIFE_ICON = "icon-life";
-    public static final String HUD_AMMO_LABEL = "Projectile: ";
+    public static final String HUD_AMMO_LABEL = "Fuel: ";
     public static final String HUD_SCORE_LABEL = "Score: ";
-    public static final String HUD_HEALTH_LABEL = "Health: ";
+    public static final String HUD_HEALTH_LABEL = "Stamina: ";
     public static final String HUD_ENERGY_LABEL = "EnergyType: ";
     public static final Vector2 ABILITY_ICON_CENTER = new Vector2(20, 8.5f);
     public static final Vector2 LIFE_ICON_CENTER = new Vector2(7, 4.5f);
