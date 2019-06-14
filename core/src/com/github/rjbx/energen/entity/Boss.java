e8065fab313f5e05a0e6f949944b6f952a77dab8
rjbx <rjbx@users.noreply.github.com>
2019-06-14 14:44:05 -0400

    refactor: Apply blade damage factor from LevelUpdater logic

--- core/src/com/github/rjbx/energen/entity/Boss.java
+++ core/src/com/github/rjbx/energen/entity/Boss.java
@@ -1004,7 +1004,7 @@ public class Boss extends Hazard implements Destructible, Humanoid, Impermeable,
         action = Action.FALLING;
         float recoveryTimeSeconds = Helpers.secondsSince(recoveryStartTime);
         if (recoveryTimeSeconds > Constants.RECOVERY_TIME) {
-            health -= hazard.getDamage() * ((hazard instanceof Blade) ? Constants.BLADE_DAMAGE_FACTOR : 1);
+            health -= hazard.getDamage() / ((hazard instanceof Blade) ? Constants.BLADE_DAMAGE_FACTOR : 1f);
             action = Action.RECOILING;
             recoveryStartTime = TimeUtils.nanoTime();
         }
