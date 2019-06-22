36e58be3b2ca83bfc5aa86d847c30835006ae487
rjbx <rjbx@users.noreply.github.com>
2019-06-22 12:27:15 -0400

    refactor: Revert ability limitation

--- core/src/com/github/rjbx/energen/entity/Avatar.java
+++ core/src/com/github/rjbx/energen/entity/Avatar.java
@@ -1292,7 +1292,7 @@ public class Avatar extends Entity implements Impermeable, Humanoid {
                     if (startTurbo == 0) {
                         startTurbo = turbo;
                     }
-                    if (!canMove && carriedGround == null && turbo >= Constants.LEAP_TURBO_DECREMENT) {
+                    if (turbo >= Constants.LEAP_TURBO_DECREMENT) {
                         turbo -= Constants.LEAP_TURBO_DECREMENT;
                     }
                 } else if (turbo < Constants.LEAP_TURBO_DECREMENT) {
