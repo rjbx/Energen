04f2ca154e833ba7a3bd3bb6310ddd08b292bf42
rjbx <rjbx@users.noreply.github.com>
2019-06-24 13:40:07 -0400

    chore: Update annotation

--- core/src/com/github/rjbx/energen/entity/Boss.java
+++ core/src/com/github/rjbx/energen/entity/Boss.java
@@ -23,10 +23,13 @@ import java.util.ListIterator;
 import static com.github.rjbx.energen.util.Enums.Orientation.X;
 
 // TODO[M]: Improve AI
-// TODO: Recolor suit to white and apply color through texture shading and projectile by type
+//    -Conditionally adjust AI for each type
+// TODO[M]: Recolor suit to white and apply color through texture shading and projectile by type
 //  Considerations:
 //    -Would need to modularize rendering as with Avatar
 //    -Alternative is to create distinct sprites for each type and provide from AssetManager a texture by type accessor
+// TODO[L]: Add tag to enable adding bosses as regular enemies i.e. in final stage
+
 // mutable
 public class Boss extends Hazard implements Destructible, Humanoid, Impermeable, Shielded {
 
