c297a91954f42f7432241dcfca44614f91d01f9d
rjbx <rjbx@users.noreply.github.com>
2019-06-28 10:01:24 -0400

    refactor: Change starting health for miniboss

--- core/src/com/github/rjbx/energen/entity/Boss.java
+++ core/src/com/github/rjbx/energen/entity/Boss.java
@@ -202,6 +202,7 @@ public class Boss extends Hazard implements Destructible, Humanoid, Impermeable,
         groundState = Enums.GroundState.AIRBORNE;
         ammo = Constants.INITIAL_AMMO;
         health = Constants.INITIAL_HEALTH;
+        if (miniBoss) health /= 2;
         turbo = Constants.MAX_TURBO;
         shotIntensity = ShotIntensity.NORMAL;
         startTurbo = turbo;
