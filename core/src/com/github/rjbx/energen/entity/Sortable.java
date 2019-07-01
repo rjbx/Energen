a543fae413142b3fade12bb6fd5473cdf131979a
rjbx <rjbx@users.noreply.github.com>
2019-06-30 22:05:51 -0400

    chore: Update annotation

--- core/src/com/github/rjbx/energen/entity/Sortable.java
+++ core/src/com/github/rjbx/energen/entity/Sortable.java
@@ -6,25 +6,40 @@ public interface Sortable {
 }
 
 /* Priority mapping
+armoroll 2
+avatar 1
+blade 2
  0/3 barrier
+ 1 boss
  2 cannon
  2 cannoroll
  2 chamber
  1 coals
  1 deposit
+ 2 effect
  1 gate
  1 ice
+ 1 impact
  1  knob
  2 ladder
  1 lava
  1 lift
+ 2 orben
  2 pillar
  2 pod
  2 pole
+ 2 portal
+ 2 powerup
+ 2 projectile
+ 2 protrusion
+ 2 rollen
  2 rope
  1 sand
  1 slick
  2 spring
+ 2 suspension
+ 1 swoopa
+ 2 teleport
  1 treadmill
  2 tripchamber
  2 tripknob
@@ -32,4 +47,5 @@ public interface Sortable {
  1 triptread
  2 vines
  1 waves
+ 1 zoomba
  */
