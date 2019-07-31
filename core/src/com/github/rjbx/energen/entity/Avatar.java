ca7c15a82f9cba7d3219b3fc3c8507679ca497c7
rjbx <bob@coded.art>
2019-07-31 13:30:37 -0400

    refactor: Deactivate climb after clearing climable

--- core/src/com/github/rjbx/energen/entity/Avatar.java
+++ core/src/com/github/rjbx/energen/entity/Avatar.java
@@ -535,8 +535,10 @@ public class Avatar extends Entity implements Impermeable, Humanoid {
                     }
                 } else if (g instanceof Pourous) {
                     setAtopGround(g); // when any kind of collision detected and not only when breaking plane of ground.top
-//                    canCling = false;
-//                    canClimb = false;
+                    if (g.getBottom() < bottom) {
+                        canCling = false;
+                        canClimb = false;
+                    }
                     canSink = true;
                     canDash = false;
 //                    canHover = false;
